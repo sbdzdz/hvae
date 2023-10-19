78242ce47beff1b7175892f2089f43a8d1c496a2
Sebastian <sebastian.dziadzio@gmail.com>
2023-10-19 16:26:35 +0200

    Use shorter runs

--- slurm/train_r1.sh
+++ slurm/train_r1.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                                                                     # Number of tasks (see below)
 #SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
 #SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
-#SBATCH --time=0-12:00                                                                 # Runtime in D-HH:MM
+#SBATCH --time=0-04:00                                                                 # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1                                                                   # Request 1 GPU
 #SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=/mnt/qb/work/bethge/dziadzio08/projects/hvae/slurm/hostname_%j.out    # File to which STDOUT will be written - make sure this is not on $HOME
